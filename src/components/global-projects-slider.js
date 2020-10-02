b0c96e3a46529e82905541395870c39e823a2282
Jejo <jejomardorongon@gmail.com>
2020-10-02 16:57:19 +0800

    Made projects slider in homepage gatsby image

--- src/components/global-projects-slider.js
+++ src/components/global-projects-slider.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Img from 'gatsby-image';
-import Slider from "react-slick";
+import React from "react"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
+import BGImage from "gatsby-background-image"
+import Slider from "react-slick"
 
-const GlobalProjectSlider = ({ ...props }) =>  {
-  const content = props.contentData;
-  const customSlider = React.createRef();
+const GlobalProjectSlider = ({ ...props }) => {
+  const content = props.contentData
+
+  const customSlider = React.createRef()
 
   const sliderSettings = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false
-  };
+    arrows: false,
+  }
 
   const gotoNext = () => {
     customSlider.current.slickNext()
@@ -25,39 +27,107 @@ const GlobalProjectSlider = ({ ...props }) =>  {
 
   return (
     <div className="project__block">
-      <div className="container" 
-      data-sal="slide-up" 
-      data-sal-easing="ease"
-      data-sal-delay="5"> 
+      <div
+        className="container"
+        data-sal="slide-up"
+        data-sal-easing="ease"
+        data-sal-delay="5"
+      >
         <h2>latest projects</h2>
-        <Slider className="project__slider" ref={customSlider} {...sliderSettings}>
-          {content.map((project) => (
+        <Slider
+          className="project__slider"
+          ref={customSlider}
+          {...sliderSettings}
+        >
+          {content.map(project => (
             <div className="slide" key={project.node.wordpress_id}>
               <div className="project__box">
                 <div className="project_image">
-                <Link to={project.node.path}>
-                  {project.node.featured_media && 
-                    <div className="bg__image" style={{ backgroundImage: `url(${project.node.featured_media.link})` }}><Img fluid={project.node.featured_media.localFile.childImageSharp.fluid} alt="Mortlock Timber" /></div>
-                  }
-                </Link>
+                  <Link to={project.node.path}>
+                    {project.node.featured_media && (
+                      <BGImage
+                        className="bg__image"
+                        fluid={
+                          project.node.featured_media.localFile.childImageSharp
+                            .fluid
+                        }
+                      >
+                        <Img
+                          fluid={
+                            project.node.featured_media.localFile
+                              .childImageSharp.fluid
+                          }
+                          alt="Mortlock Timber"
+                        />
+                      </BGImage>
+                    )}
+                  </Link>
                 </div>
                 <div className="project_text">
-                  <span className="project_title"><Link to={project.node.path} dangerouslySetInnerHTML={{ __html: project.node.title }} /></span>
+                  <span className="project_title">
+                    <Link
+                      to={project.node.path}
+                      dangerouslySetInnerHTML={{ __html: project.node.title }}
+                    />
+                  </span>
                 </div>
                 <div className="control_wrapper">
-                  <button className="slick-arrow slick-prev" onClick={()=>gotoPrev()}><span className="text">Prev</span><svg className="icon" width="100pt" height="100pt" version="1.1" viewBox="0 0 100 100"><path d="m87.5 45.832h-58.75l17.918-17.914-5.8359-5.8359-27.914 27.918 27.914 27.918 5.8359-5.8359-17.918-17.914h58.75z"/></svg></button>
-                  <button className="slick-arrow slick-next" onClick={()=>gotoNext()}><span className="text">Next</span><svg className="icon" width="100pt" height="100pt" version="1.1" viewBox="0 0 100 100"><path d="m12.5 45.832h64.582v8.332h-64.582z"/><path d="m59.168 77.918l-5.8359-5.8359 22.086-22.082-22.086-22.082 5.8359-5.8359 27.914 27.918z"/></svg></button>
+                  <button
+                    className="slick-arrow slick-prev"
+                    onClick={() => gotoPrev()}
+                  >
+                    <span className="text">Prev</span>
+                    <svg
+                      className="icon"
+                      width="100pt"
+                      height="100pt"
+                      version="1.1"
+                      viewBox="0 0 100 100"
+                    >
+                      <path d="m87.5 45.832h-58.75l17.918-17.914-5.8359-5.8359-27.914 27.918 27.914 27.918 5.8359-5.8359-17.918-17.914h58.75z" />
+                    </svg>
+                  </button>
+                  <button
+                    className="slick-arrow slick-next"
+                    onClick={() => gotoNext()}
+                  >
+                    <span className="text">Next</span>
+                    <svg
+                      className="icon"
+                      width="100pt"
+                      height="100pt"
+                      version="1.1"
+                      viewBox="0 0 100 100"
+                    >
+                      <path d="m12.5 45.832h64.582v8.332h-64.582z" />
+                      <path d="m59.168 77.918l-5.8359-5.8359 22.086-22.082-22.086-22.082 5.8359-5.8359 27.914 27.918z" />
+                    </svg>
+                  </button>
                 </div>
               </div>
             </div>
           ))}
         </Slider>
         <div className="button_wrapper">
-          <Link to="/portfolio" className="button-learn white">View all projects <span className="btnArrow"><svg className="icon" width="100pt" height="100pt" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path d="m32.812 0l-15.625 15.625 34.375 34.375-34.375 34.375 15.625 15.625 50-50z"/></svg></span></Link>
+          <Link to="/portfolio" className="button-learn white">
+            View all projects{" "}
+            <span className="btnArrow">
+              <svg
+                className="icon"
+                width="100pt"
+                height="100pt"
+                version="1.1"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m32.812 0l-15.625 15.625 34.375 34.375-34.375 34.375 15.625 15.625 50-50z" />
+              </svg>
+            </span>
+          </Link>
         </div>
       </div>
     </div>
   )
 }
 
-export default GlobalProjectSlider;
+export default GlobalProjectSlider
--- src/components/productsingle/product-species.js
+++ src/components/productsingle/product-species.js
@@ -1,51 +1,86 @@
-import React, { useState } from 'react';
-import Img from 'gatsby-image';
+import React, { useState } from "react"
+import Img from "gatsby-image"
 
-import ProductFinishes from './product-finishes-popup';
+import ProductFinishes from "./product-finishes-popup"
 
 const togglePopupOverlay = () => {
-  document.body.classList.toggle('popup__active');
+  document.body.classList.toggle("popup__active")
 }
 
-const ProductSpecies = ({ ...props }) =>  {
-  const content = props.data;
-  const [popupIndex, setPopupIndex] = useState();
-  const [popupTitle, setPopupTitle] = useState();
-  const [popupActive, setpopupActive] = useState();
+const ProductSpecies = ({ ...props }) => {
+  const content = props.data
+  const [popupIndex, setPopupIndex] = useState()
+  const [popupTitle, setPopupTitle] = useState()
+  const [popupActive, setpopupActive] = useState()
 
   const setFinishIndex = (num, title) => {
-    togglePopupOverlay();
-    setPopupIndex(num);
-    setPopupTitle(title);
-    setpopupActive(true);
+    togglePopupOverlay()
+    setPopupIndex(num)
+    setPopupTitle(title)
+    setpopupActive(true)
   }
-  
+
   return (
     <div className="product-species">
       <div className="general-heading">
         <h2>timber species</h2>
-        <span className="info">Select timber species below to see their corresponding finishes</span>
+        <span className="info">
+          Select timber species below to see their corresponding finishes
+        </span>
       </div>
       <div className="species-boxes">
         <div className="row">
-          {content.species ? content.species.map((timber, index) => (
-            <div className="col-xs-6 col-sm-3" data-sal="slide-up" 
-            data-sal-easing="ease"
-            data-sal-delay="5" data-key={`${index}`} key={index}>
-              <div className="species-box">
-                <div className="image">
-                  { timber.timber_small_thumbnail ? <Img fluid={timber.timber_small_thumbnail.localFile.childImageSharp.fluid} alt={timber.timber_small_thumbnail.alt_text || ''} /> : null }
-                  <span role="button" tabIndex={0} className="button whiteoutline" onKeyDown={ () => setFinishIndex(index, timber.timber_title) } onClick={ () => setFinishIndex(index, timber.timber_title) }>View finishes</span>
+          {content.species
+            ? content.species.map((timber, index) => (
+                <div
+                  className="col-xs-6 col-sm-3"
+                  data-sal="slide-up"
+                  data-sal-easing="ease"
+                  data-sal-delay="5"
+                  data-key={`${index}`}
+                  key={index}
+                >
+                  <div className="species-box">
+                    <div className="image">
+                      {timber.timber_small_thumbnail ? (
+                        <Img
+                          fluid={
+                            timber.timber_small_thumbnail.localFile
+                              .childImageSharp.fluid
+                          }
+                          alt={timber.timber_small_thumbnail.alt_text || ""}
+                        />
+                      ) : null}
+                      <span
+                        role="button"
+                        tabIndex={0}
+                        className="button whiteoutline"
+                        onKeyDown={() =>
+                          setFinishIndex(index, timber.timber_title)
+                        }
+                        onClick={() =>
+                          setFinishIndex(index, timber.timber_title)
+                        }
+                      >
+                        View finishes
+                      </span>
+                    </div>
+                    <span className="title">{timber.timber_title}</span>
+                  </div>
                 </div>
-                <span className="title">{timber.timber_title}</span>
-              </div>
-            </div>
-          )) : null }
+              ))
+            : null}
         </div>
       </div>
-      { (popupIndex !== undefined && popupActive) ? <ProductFinishes title={popupTitle} data={content.species[popupIndex].timber_finishes} link={content.timber_finishes_download_link} /> : null }
+      {popupIndex !== undefined && popupActive ? (
+        <ProductFinishes
+          title={popupTitle}
+          data={content.species[popupIndex].timber_finishes}
+          link={content.timber_finishes_download_link}
+        />
+      ) : null}
     </div>
   )
 }
 
-export default ProductSpecies;
+export default ProductSpecies
